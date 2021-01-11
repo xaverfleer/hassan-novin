a7d682734c5aa0356439e7baf211ab43c3b93ebd
Xaver Fleer <xaver.fleer@gmail.com>
2021-01-11 22:42:50 +0100

    Simplify function

--- api/sendMessage.js
+++ api/sendMessage.js
@@ -1,23 +1,24 @@
-const logging = require('./private/logging')
-const mailing = require('./private/mailing')
-const responding = require('./private/responding')
+// const logging = require('./private/logging')
+// const mailing = require('./private/mailing')
+// const responding = require('./private/responding')
 
-exports.handler = function absenden(event, context, callback) {
-  logging.logStart('Start sendMessage.js')
-  const respond = responding.responseHandlers(callback)
+module.exports = function absenden(req, res) {
+  // logging.logStart('Start sendMessage.js')
+  // const respond = responding.responseHandlers(callback)
 
-  const parsedBody = JSON.parse(event.body)
+  // const parsedBody = JSON.parse(event.body)
 
-  const msg = {
-    to: parsedBody.recipient,
-    subject: 'Nachricht von Website gesendet',
-    text: JSON.stringify(parsedBody.formData),
-  }
+  // const msg = {
+  //   to: parsedBody.recipient,
+  //   subject: 'Nachricht von Website gesendet',
+  //   text: JSON.stringify(parsedBody.formData),
+  // }
 
-  logging.log('sending email')
-  mailing
-    .sendEmail(msg)
-    .then(() => 'success')
-    .catch(logging.logAndReject)
-    .then(respond.success, respond.failed)
+  // logging.log('sending email')
+  // mailing
+  //   .sendEmail(msg)
+  //   .then(() => 'success')
+  //   .catch(logging.logAndReject)
+  //   .then(respond.success, respond.failed)
+  return res.json({ body: req.body, query: req.query })
 }
